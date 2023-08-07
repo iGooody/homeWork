d4e90097489c739e16ae31fe25b16c8bd469100d
Anton <borzenkov0anton@gmail.com>
2023-08-07 17:03:56 +0200

    first done

--- homeWork1.js
+++ homeWork1.js
@@ -1,11 +1,30 @@
-let first = prompt('Enter first number');
-let second = +(prompt('Enter second number'));
+let first = +prompt('Enter first number');
+let second = +prompt('Enter second number');
 
-function getTwoNumbers(first, second) {
-    if (!Number.isFinite(first) || !Number.isFinite(second)) {
-        alert('Incorrect input');
-        return Error;
+function numbersBaseChange(first, second) {
+    if (
+        Number.isFinite(first) &&
+        Number.isFinite(second) &&
+        second <= 36 && second >= 2
+    ) {
+        console.log(first.toString(second));
+    } else {
+        console.log("Некорректный ввод!");
     }
-    return parseInt(first, second); 
 }
-console.log(getTwoNumbers(first, second));
+
+numbersBaseChange(first, second);
+
+
+let value1 = +prompt('Enter first value');
+let value2 = +prompt('Enter second value');
+
+function sumAndDivisionResult(value1, value2) {
+    if (!Number.isFinite(value1) || !Number.isFinite(value2)) {
+        console.log("Некорректный ввод!");
+    } else {
+        console.log(`Ответ: ${value1 + value2}, ${value1 / value2}`);
+    }
+}
+
+sumAndDivisionResult(value1, value2);
